6f4ee771644df338bd1e5064d6c329e2c2f24e83
ThomasGorisse <ThomasGorisse@users.noreply.github.com>
2021-10-18 21:37:34 +0000

    Dokka docs

---
+++ docs/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies='{":android-view-lifecycle:dokkaHtml/androidTestRelease":[],":android-view-lifecycle:dokkaHtml/debug":[],":android-view-lifecycle:dokkaHtml/main":[],":android-view-lifecycle:dokkaHtml/release":[]}'
